2dd5de0cf2b9a636717a81dc0c03825ad37d1dfc
Dave Inoc <michael828inoc@gmail.com>
2021-06-14 15:59:53 +0800

    added heya

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>
-    <h1>Footer</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Footer</h1>
+      <h1>Heya</h1>
+    </div>
+  );
 };
 
 export default Footer;
